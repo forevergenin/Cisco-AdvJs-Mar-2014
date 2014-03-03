24752dd2796319065666d23e408ea4e0658977d2
tkmagesh <tkmagesh77@gmail.com>
2014-03-03 10:46:30 -0800

    Language Exploration - p2

--- Day-1/01.ExploringJavaScript/calculator.js
+++ Day-1/01.ExploringJavaScript/calculator.js
@@ -1,3 +1,18 @@
-function sum(x,y){
-	return x + y;
+function sum(){
+	function parseArg(n){
+		if (!isNaN(n)) { return  parseInt(n); }
+		if (typeof n === "function") return parseArg(n());
+		if (n instanceof Array){
+			var result  = 0;
+			for(var i=0;i<n.length;i++)
+				result += parseArg(n[i]);
+			return result;
+		}
+		return  0;	
+	}
+	var result  = 0;
+	for(var i=0;i<arguments.length;i++)
+		result += parseArg(arguments[i]);
+	return result;
+	
 }
--- Day-1/01.ExploringJavaScript/calculatorSpecs.js
+++ Day-1/01.ExploringJavaScript/calculatorSpecs.js
@@ -8,5 +8,62 @@ window.addEventListener("DOMContentLoaded",function(){
 
 		//Assert
 		return result === expectedResult;
-	})
+	});
+	test("Should be able to add two numbers in string format",function(){
+		//Arrange
+		var number1 = "10", number2 = "20", expectedResult = 30;
+
+		//Act
+		var result = sum(number1,number2);
+
+		//Assert
+		return result === expectedResult;
+	});
+	test("Should be able to add two functions returning numbers",function(){
+		//Arrange
+		var f1 = function(){ return 10;};
+		var f2 = function(){ return 20;};
+		var expectedResult = 30;
+		//Act
+		var result = sum(f1,f2);
+
+		//Assert
+		return result === expectedResult;
+	});
+	test("Should be able to add two functions returning numbers in string format",function(){
+		//Arrange
+		var f1 = function(){ return "10";};
+		var f2 = function(){ return "20";};
+		var expectedResult = 30;
+		//Act
+		var result = sum(f1,f2);
+
+		//Assert
+		return result === expectedResult;
+	});
+	test("Should be able to add more than two arguments",function(){
+		//Arrange
+		var number1 = 10,
+			number2 = 20,
+			number3 = 30;
+		var expectedResult = 60;
+		//Act
+		var result = sum(number1, number2, number3);
+
+		//Assert
+		return result === expectedResult;
+	});
+
+	test("Should be able to add an array of numbers",function(){
+		//Arrange
+		var numbers = [10,20,30];
+		var expectedResult = 60;
+		//Act
+		var result = sum(numbers);
+
+		//Assert
+		return result === expectedResult;
+	});
+	
+
 })
