9976007da41353cc7038bbf04bcbca9323159141
tkmagesh <tkmagesh77@gmail.com>
2014-03-03 16:18:34 -0800

    Function Memoization using Closures

--- Day-1/04.FunctionMemoization/isPrime.js
+++ Day-1/04.FunctionMemoization/isPrime.js
@@ -12,3 +12,23 @@ function isPrime(n){
      console.log("result from cache ", isPrime[n]);
   }
 }
+
+//Using Closures
+function getPrimer(){
+  var result = {};
+  return function(n){
+  if (typeof result[n] === "undefined"){
+       
+       result[n] = true;
+       for(var i=2;i<=(n/2);i++){
+          if (n % i === 0) {
+            result[n] = false;
+            break;
+          }
+        }
+       console.log("brewing the result ", result[n]);
+  } else {
+     console.log("result from cache ", result[n]);
+  }
+}
+}
